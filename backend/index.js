f77d8f78e9d392b6285b1e2276ca2ed9f691b432
Parth <parthmaindola@gmail.com>
2024-07-27 22:10:47 +0530

    added pre install

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require('axios');
 
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -19,8 +20,8 @@ app.post("/authenticate", async (req, res) => {
     } catch (e) {
         return res.status(e.response.status).json(e.response.data);
     }
-
 });
+
 
 app.listen(3001);
 
